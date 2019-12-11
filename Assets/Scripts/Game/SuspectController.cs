b0d424e31e14f4fcab16a7cc6d808fa4b2868ce9
Nicolas Kniebihler <nicolask77@free.fr>
2019-12-11 14:49:49 +0100

    Adding BoxCollider2D dependency to SuspectController

--- Assets/Scripts/Game/SuspectController.cs
+++ Assets/Scripts/Game/SuspectController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class SuspectController : MonoBehaviour
 {
     // Start is called before the first frame update
