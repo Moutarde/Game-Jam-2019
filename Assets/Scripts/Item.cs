cd5af95fcec12bb9062a1bce3466bc02450d63e8
SebastienDiGiovanni <sebastien.di-giovanni@insa-lyon.fr>
2019-12-11 10:55:42 +0100

    First implementation of Item and ItemsDatabase

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public enum ItemType
+    {
+        Type0,
+        Type1,
+        Type2
+    };
+
+    public ItemType m_itemType;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item/SimpleItem", order = 1)]
+public class SimpleItem : Item
+{
+    public Sprite m_sprite;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "Item/ComplexItem", order = 2)]
+public class ComplexItem : Item
+{
+    public Sprite m_sprite0;
+    public Sprite m_sprite1;
+    public Sprite m_sprite2;
+    public Sprite m_sprite3;
+}
