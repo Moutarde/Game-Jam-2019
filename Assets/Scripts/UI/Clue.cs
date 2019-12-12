9e9c233fa95d826832ae2df3bae2882c62f3c6fc
Nicolas Kniebihler <nicolask77@free.fr>
2019-12-12 12:09:10 +0100

    Add dependencies on clues and scores scripts

--- Assets/Scripts/UI/Clue.cs
+++ Assets/Scripts/UI/Clue.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class Clue : MonoBehaviour
 {
     TextMeshProUGUI m_textMesh;
--- Assets/Scripts/UI/PlayerScore.cs
+++ Assets/Scripts/UI/PlayerScore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class PlayerScore : MonoBehaviour
 {
     TextMeshProUGUI m_textMesh;
